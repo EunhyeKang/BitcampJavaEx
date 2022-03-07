d1942211dbb55ce1c99995b6f590c57693677a95
Eunhye Kang <gracekang61@naver.com>
2022-03-07 22:59:09 +0900

    실습중

--- Ch02_DataType/src/sec02/primitive/Ex03_Byte.java
+++ Ch02_DataType/src/sec02/primitive/Ex03_Byte.java
@@ -2,11 +2,23 @@ package sec02.primitive;
 
 public class Ex03_Byte {
 	
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		
-	//	system.
-		
-		
-		
+
+		System.out.println("byte 최대값 : " + Byte.MAX_VALUE);
+		System.out.println("byte 최대값 : " + Byte.MIN_VALUE);
+
+		byte var1 = -128;
+		byte var2 = -30;
+		byte var3 = 0;
+		byte var4 = 30;
+		byte var5 = 127;
+
+		short var6 = 128;
+		int var7 = 128;
+		long var8 = 128;
+
+		System.out.println(var6);
+
 	}
 }
